e88d0388ad48f6c753e062ca1b831259778aee17
PJC-99 <placido.jorgecollet@kaust.edu.sa>
2024-09-30 15:52:06 +0300

    Update gene_finder.py

--- gene_finder.py
+++ gene_finder.py
@@ -1,1 +1,6 @@
+#Gene Finder tool
+###LLM : Chatgpt-4o
+###Prompt: "Write a python code to find a single gene in a sequence from a input file in fasta (.fna) format. 
+###The output must be a region between start codon 'ATG' and stop codons 'TAA', 'TAG', 'TGA' inside the fasta file and consider three possible reading frames."
+
 
