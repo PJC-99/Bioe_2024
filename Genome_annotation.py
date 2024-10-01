1608bc10bd52b12b3289960bf3a21e97ca43ef4d
PJC-99 <placido.jorgecollet@kaust.edu.sa>
2024-10-01 17:45:24 +0300

    Update and rename Week 4+5.py to Genome_annotation.py

---
+++ Genome_annotation.py
@@ -0,0 +1,13 @@
+# Given the amino acid sequence:  KVRMFTSELDIMLSVNGPADQIKYFCRHWT*
+### What is the number of amino acids in the encoded
+### peptide (not including the stop codon)? Additonally, how many bases are
+### contained in the open reading frame of the DNA sequence encoding the
+### amino acids (including the stop codon)
+
+import numpy as np
+
+def count_aminoacids(seq):
+aminoacids = []
+
+for aminoacid in aminoacids:
+    
